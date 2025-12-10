e2133fe239b07b1228aa6c3a5ef8e9e07fe6ea5e
Cyan <cyanideinformation@gmail.com>
2025-12-09 20:40:34 -0400

    Add placeholder pages for stats and wall

---
+++ pages/stats/index.js
@@ -0,0 +1,8 @@
+export default function Stats() {
+  return (
+    <div>
+      <h1>Stats Page</h1>
+      <p>Stats content coming soon!</p>
+    </div>
+  );
+}
---
+++ pages/wall/index.js
@@ -0,0 +1,8 @@
+export default function Wall() {
+  return (
+    <div>
+      <h1>Wall of Fame</h1>
+      <p>Wall content coming soon!</p>
+    </div>
+  );
+}
