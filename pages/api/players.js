a5e3e0cadfc0458a514c6e79b62ff03254eaaae3
Cyan <cyanideinformation@gmail.com>
2025-12-27 20:38:17 -0400

    Working on player list

--- pages/api/players.js
+++ pages/api/players.js
@@ -3,14 +3,20 @@ import mysql from 'mysql2/promise';
 export default async function handler(req, res) {
   let connection;
 
-  try {
-    connection = await mysql.createConnection({
-      host: '127.0.0.1',       // force IPv4
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+try {
+  connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  console.log("Connected to DB!");
+} catch (err) {
+  console.error("DB connection failed:", err);
+}
+
+
 
     const [rows] = await connection.execute('SELECT * FROM player');
     res.status(200).json(rows);
